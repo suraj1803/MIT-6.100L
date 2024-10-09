9cb1959718a482a96c39ad401929a00ee79e2cce
Suraj Biswas <surajbiswas182003@gmail.com>
2024-10-09 19:26:33 +0530

    intial commit

---
+++ mit6_100l_f22_ps1_code/ps1a.py
@@ -0,0 +1,21 @@
+## 6.100A PSet 1: Part A
+## Name: Suraj Biswas
+## Time Spent:
+## Collaborators:
+
+##################################################################################
+## Get user input for yearly_salary, portion_saved and cost_of_dream_home below ##
+##################################################################################
+yearly_salary = float(input("Enter your yearly salary: "))
+portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
+cost_of_dream_home =  float(input("Enter the cost of your dream home: "))
+
+
+#########################################################################
+## Initialize other variables you need (if any) for your program below ##
+#########################################################################
+portion_down_payment = 0.25
+
+###############################################################################################
+## Determine how many months it would take to get the down payment for your dream home below ## 
+###############################################################################################
